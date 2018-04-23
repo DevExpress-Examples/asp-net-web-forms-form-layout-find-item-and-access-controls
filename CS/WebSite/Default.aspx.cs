cc0eb336d5973ef687dba204b8e85712c5c78e0e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxEditors;
+using DevExpress.Web.ASPxFormLayout;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) { }
+    protected void ASPxButton1_Click(object sender, EventArgs e) {
+        LayoutItemBase item = layout.FindItemByPath(pathTextBox.Text) as LayoutItemBase;
+        UpdateItem(item);
+    }
+    private void UpdateItem(LayoutItemBase item) {
+        if(item is LayoutItem)
+            SetValue(item as LayoutItem);
+        else
+            if(item is LayoutGroupBase)
+                (item as LayoutGroupBase).ForEach(IterateNestedControls);
+            else {
+                pathTextBox.IsValid = false;
+                pathTextBox.ValidationSettings.ErrorText = "There is no available group for this path";
+            }
+    }
+    void IterateNestedControls(LayoutItemBase item) {
+        foreach(LayoutItemBase c in item.Collection) {
+            if(c is LayoutGroup)
+                (c as LayoutGroup).ForEach(IterateNestedControls);
+            if(c is LayoutItem)
+                SetValue(c as LayoutItem);
+        }
+    }
+    static void SetValue(LayoutItem c) {
+        foreach(Control control in c.LayoutItemNestedControlContainer.Controls) {
+            ASPxEdit editor = control as ASPxEdit;
+            if(editor != null)
+                editor.Value = DateTime.Now.ToString();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# ASPxFormLayout - How to get an item using FindItemByPath method
+
+
+<p>This example shows how to access a single item by using <strong><a href="http://documentation.devexpress.com/#AspNet/DevExpressWebASPxFormLayoutLayoutGroupBase_FindItemByPathtopic"><u>LayoutGroupBase.FindItemByPath</u></a></strong> method.<br><br><strong>Updated:</strong><br><br>The<strong> <a href="https://documentation.devexpress.com/#AspNet/DevExpressWebASPxFormLayout_FindItemOrGroupByNametopic">ASPxFormLayout.FindItemByPath</a></strong> method and the <strong><a href="https://documentation.devexpress.com/#AspNet/DevExpressWebLayoutItem_LayoutItemNestedControlContainertopic">LayoutItem.LayoutItemNestedControlContainer</a> </strong>property are obsolete. It is recommended to use the <strong><a href="https://documentation.devexpress.com/#AspNet/DevExpressWebASPxFormLayout_FindItemOrGroupByNametopic">ASPxFormLayout.FindItemOrGroupByName</a> </strong>method and the <strong><a href="https://documentation.devexpress.com/#AspNet/DevExpressWebLayoutItem_Controlstopic">LayoutItem.Controls</a> </strong>property instead.</p>
+
+<br/>
+
+
